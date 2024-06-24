fd9f82e066dacd7e63d573783cccab61423a71e4
Ananya Ghoshal <ananyaghoshal.20@gmail.com>
2024-06-24 08:58:40 +0530

    updated details section on home page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -37,25 +37,52 @@ function HomePage() {
           <span className="homepage__header__details">
             <Typewriter text="a frontend developer !" delay={200} />
             <br />
-            <span className="details">
-              I turn designs into reality by building responsive websites that
-              are visually pleasing and provide a rich browsing experience.
-              Front-end development is a field of constant innovation, with new
-              technologies emerging all the time. This continuous evolution
-              fuels my passion for learning and keeps me motivated.
-            </span>
+            <div className="homepageButtons">
+              <Link to="aboutMe" smooth={true}>
+                <button className="button__about">Know more about me</button>
+              </Link>
+              <a href={Resume} target="_blank">
+                <button className="CV">
+                  <img src={ResumeIcon} />
+                  Resume
+                </button>
+              </a>
+            </div>
+            <div
+              className="details"
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                gap: "0.5rem",
+              }}
+            >
+              <p>
+                I am a Computer Science Engineer with three years of industry
+                experience in frontend development. Throughout my career, I have
+                collaborated with various clients and worked on diverse
+                projects. I have built desktop applications from scratch and
+                implemented UI changes to meet design requirements.
+              </p>
+              {/* <p>
+                I turn designs into reality by building responsive websites that
+                are visually pleasing and provide a rich browsing experience.
+                Front-end development is a field of constant innovation, with
+                new technologies emerging all the time. This continuous
+                evolution fuels my passion for learning and keeps me motivated.
+              </p>
+              <p>
+                I love diving deep into how the web works, and have honed my
+                skills in building products from scratch and crafting sleek web
+                UIs. My toolkit includes React, Redux,core JavaScript, HTML and
+                CSS.
+              </p>
+              <p>
+                When I'm not building websites, you'll find me immersed in
+                music—whether I'm creating my own tunes, singing the latest
+                hits, or seeking ways to rekindle my love for nature.
+              </p> */}
+            </div>
           </span>
-          <div className="homepageButtons">
-            <Link to="aboutMe" smooth={true}>
-              <button className="button__about">Know more about me</button>
-            </Link>
-            <a href={Resume} target="_blank">
-              <button className="CV">
-                <img src={ResumeIcon} />
-                Resume
-              </button>
-            </a>
-          </div>
         </div>
         <div className="homepage__right">
           <div className="aboutMe__img">
